4049ade4f24e2dbdfd155aefcfd057d933ce17bc
Douglas José <douglassousawork@gmail.com>
2025-04-20 19:59:28 -0300

    Tiro básico da nave com o clique esquerdo do mouse
    Cena da bala criada e posição do tiro que sai da nave

--- Scenes/player_teste.gd
+++ Scenes/player_teste.gd
@@ -1,11 +1,27 @@
 extends CharacterBody2D
 @export var speed = 400
 
+var bullet_path=preload("res://Scenes/bullet.tscn")
+
 func get_input():
 	look_at(get_global_mouse_position())
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
+
+
 func _physics_process(_delta):
+	
+	look_at(get_global_mouse_position())
+	if Input.is_action_just_pressed("left_click"):
+		fire()
+	
 	get_input()
 	move_and_slide()
+
+func fire():
+	var bullet=bullet_path.instantiate()
+	bullet.dir=rotation
+	bullet.pos=$Node2D.global_position
+	bullet.rota=global_rotation
+	get_parent().add_child(bullet)
---
+++ Scripts/bullet.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+var pos:Vector2
+var rota:float
+var dir : float
+var speed= 1500
+
+func _ready():
+	global_position=pos
+	global_rotation=rota
+func _physics_process(_delta):
+	velocity=Vector2(speed,0).rotated(dir)
+	move_and_slide()
