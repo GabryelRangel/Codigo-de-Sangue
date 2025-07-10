0fd9dd9aa36598613dc4e68b7b44e4334b7037af
GabryelRangel <gabryelrangelsouza@gmail.com>
2025-07-10 03:56:57 -0300

    Assets novos

--- Scripts/Arena1.gd
+++ Scripts/Arena1.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+@onready var music_player = $AudioStreamPlayer
+var fade_in_time := 2.0  # segundos para o fade in
+var delay := 1.5         # segundos de atraso antes de começar
+
 var enemy_1=preload("res://Scenes/inimigo_1.tscn")
 var enemy_2=preload("res://Scenes/inimigo_2.tscn")
 var enemy_3=preload("res://Scenes/inimigo_3.tscn")
@@ -8,6 +12,12 @@ var enemy_5=preload("res://Scenes/inimigo_5.tscn")
 
 func _ready():
 	Global.node_creation_parent = self
+	music_player.volume_db = -80  # começa silencioso
+	await get_tree().create_timer(delay).timeout
+	music_player.play()
+	# Fade in
+	var tween = create_tween()
+	tween.tween_property(music_player, "volume_db", 0, fade_in_time)
 
 func _exit_tree():
 	Global.node_creation_parent = self
